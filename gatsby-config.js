15d00b482dee7051ca67a57b42a9f4aa5ddbc284
Julien Sanmartin <julien.sanmartin@parsable.com>
2020-10-27 10:26:03 -0700

    master: fix typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Julien Sannmartin`,
-    description: `Perosnal Site`,
+    description: `Personal Site`,
     author: `@juliensanmartin`,
   },
   plugins: [
